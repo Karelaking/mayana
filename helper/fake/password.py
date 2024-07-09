983d16ebc37f3a492291b40f64ef57c8aa717834
mradul kumar katiyar <karelaking277@gmail.com>
2024-07-09 22:25:04 +0530

    Completed password.py

--- helper/fake/password.py
+++ helper/fake/password.py
@@ -1,114 +1,108 @@
-# from typing import List
-# from secrets import choice
-# from random import shuffle
-# from string import ascii_lowercase, ascii_uppercase, digits, punctuation
-
-
-# def random_password(
-#         count: int = 1,
-#         length: int = 10,
-#         is_lower_case: bool = True,
-#         is_upper_case: bool = False,
-#         is_digits: bool = False,
-#         is_punctuation: bool = False,
-# ) -> str | List[str]:
-#     """
-#     Generates the random password
-
-#     :param count: No. of output passwords
-#     :param length: Length of the single password
-#     :param is_lower_case: Use lower case latter's
-#     :param is_upper_case: Use upper case latter's
-#     :param is_digits: Use digits
-#     :param is_punctuation: User punctuations
-#     :return: returns the single or list of passwords
-
-#     rtype:
-#     """
-
-#     # creates a string to generate random password
-#     LATTERS: str = ""
-#     if is_digits:
-#         LATTERS += digits
-#     if is_upper_case:
-#         LATTERS += ascii_uppercase
-#     if is_lower_case:
-#         LATTERS += ascii_lowercase
-#     if is_punctuation:
-#         LATTERS += punctuation
-
-#     if count != 1:
-#         password: List[str] = []
-
-#         for _ in range(count):
-#             shuffle(list(LATTERS))
-#             try:
-#                 password.append("".join(choice(LATTERS) for _ in range(length)))
-#             except:
-#                 raise IndexError("")
-#         return password
-
-#     else:
-#         try:
-#             shuffle(list(LATTERS))
-#             return "".join(choice(LATTERS) for _ in range(length))
-#         except:
-#             raise IndexError("")
-
-
-import random
+# Imports
+from secrets import choice
 import string
+from typing import List
 import re
+
+
+# Generate the password using random choices from the pool
+def generate_random_password(
+    char_pool: str,
+    length: int,
+    is_digits: bool,
+    is_lower: bool,
+    is_upper: bool,
+    is_punctuation: bool,
+):
+    password = "".join(choice(char_pool) for _ in range(length))
+
+    # Ensuring at least one character from each selected pool is in the password
+    while True:
+        password_valid = True
+        if is_digits and not re.search(r"\d", password):
+            password_valid = False
+        if is_lower and not re.search(r"[a-z]", password):
+            password_valid = False
+        if is_upper and not re.search(r"[A-Z]", password):
+            password_valid = False
+        if is_punctuation and not re.search(
+            r"[!\"#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~]", password
+        ):
+            password_valid = False
+
+        if password_valid:
+            return password
+        else:
+            generate_random_password(
+                char_pool=char_pool,
+                is_digits=is_digits,
+                is_lower=is_lower,
+                is_upper=is_upper,
+                is_punctuation=is_punctuation,
+                length=length,
+            )
 
 
 def password(
     length=12,
-    is_use_digits=False,
-    is_use_lower_case=True,
-    is_use_upper_case=False,
-    is_use_punctuation=False,
-):
+    count: int = 1,
+    is_digits=False,
+    is_lower=True,
+    is_upper=False,
+    is_punctuation=False,
+) -> str | List[str]:
+    """
+    Generates the random password
+
+    :param count: No. of output passwords
+    :param length: Length of the single password
+    :param is_lower: Use lower case latter's
+    :param is_upper: Use upper case latter's
+    :param is_digits: Use digits
+    :param is_punctuation: User punctuations
+    :return: returns the single or list of passwords
+
+    rtype:
+    """
     if length <= 0:
         raise ValueError("Password length must be greater than 0")
 
     # Create character pools based on parameters
     char_pools = []
-    if is_use_digits:
+    if is_digits:
         char_pools.append(string.digits)
-    if is_use_lower_case:
+    if is_lower:
         char_pools.append(string.ascii_lowercase)
-    if is_use_upper_case:
+    if is_upper:
         char_pools.append(string.ascii_uppercase)
-    if is_use_punctuation:
+    if is_punctuation:
         char_pools.append(string.punctuation)
 
     if not char_pools:
         raise ValueError("At least one character pool must be selected")
 
     # Concatenate all character pools into one string
-    all_chars = "".join(char_pools)
+    char_pool = "".join(char_pools)
 
-    # Generate the password using random choices from the pool
-    password = "".join(random.choice(all_chars) for _ in range(length))
+    if count != 1:
+        return [
+            generate_random_password(
+                char_pool=char_pool,
+                is_digits=is_digits,
+                is_lower=is_lower,
+                is_upper=is_upper,
+                is_punctuation=is_punctuation,
+                length=length,
+            )
+            for _ in range(count)
+        ]
 
-    # Ensuring at least one character from each selected pool is in the password
-    while True:
-        password_valid = True
-        if is_use_digits and not re.search(r"\d", password):
-            password_valid = False
-        if is_use_lower_case and not re.search(r"[a-z]", password):
-            password_valid = False
-        if is_use_upper_case and not re.search(r"[A-Z]", password):
-            password_valid = False
-        if is_use_punctuation and not re.search(
-            r"[!\"#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~]", password
-        ):
-            password_valid = False
-
-        if password_valid:
-            break
-        else:
-            password = "".join(random.choice(all_chars) for _ in range(length))
-
-    return password
-
+    else:
+        return generate_random_password(
+            char_pool=char_pool,
+            is_digits=is_digits,
+            is_lower=is_lower,
+            is_upper=is_upper,
+            is_punctuation=is_punctuation,
+            length=length,
+        )
