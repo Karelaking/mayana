dc82926cc856b9dab94c4ce11fe249e8515535c8
mradul kumar katiyar <karelaking277@gmail.com>
2024-07-09 21:51:02 +0530

    Completed phone.py

--- helper/fake/phone.py
+++ helper/fake/phone.py
@@ -2,36 +2,49 @@ from typing import List
 from random import randint
 import re
 
+# Define patterns for different placeholders
+patterns = {
+    "X": lambda: str(randint(0, 9)),
+    "N": lambda: str(randint(2, 9)),  # For non-zero leading digit area codes
+    "Z": lambda: str(randint(1, 9)),  # For country codes
+}
 
-def phone(format_string="+ZZ XXX-XXX-XXXX"):
-    # Define patterns for different placeholders
-    patterns = {
-        "X": lambda: str(randint(0, 9)),
-        "N": lambda: str(randint(2, 9)),  # For non-zero leading digit area codes
-        "Z": lambda: str(randint(1, 9)),  # For country codes
-    }
 
-    # Generate the phone number based on the format string
+# Generate the phone number based on the format string
+def generate_random_phone(format_string: str, patterns: dict) -> str:
     phone_number = ""
     for char in format_string:
         if char in patterns:
             phone_number += patterns[char]()
         else:
             phone_number += char
-
     return phone_number
 
 
-# Example usage
-formats = [
-    "XXX-XXX-XXXX",
-    "(XXX) XXX-XXXX",
-    "XXX.XXX.XXXX",
-    "XXX XXX XXXX",
-    "+X-XXX-XXX-XXXX",
-    "+XX-XXXX-XXXX",
-    "+Z-XX-XXX-XXX-XXXX",
-    "+ZZ-XX-XXXX-XXXX",
-]
+def phone(format_string: str = "+ZZ XXX-XXX-XXXX", count: int = 1) -> str | List[str]:
 
-print(phone())
+    if count != 1:
+        return [
+            generate_random_phone(format_string=format_string, patterns=patterns)
+            for _ in range(count)
+        ]
+
+    else:
+        return generate_random_phone(format_string=format_string, patterns=patterns)
+
+
+if __name__ == "__main__":
+    print(phone())
+
+# Example usage
+# formats = [
+#     "XXX-XXX-XXXX",
+#     "(XXX) XXX-XXXX",
+#     "XXX.XXX.XXXX",
+#     "XXX XXX XXXX",
+#     "+X-XXX-XXX-XXXX",
+#     "+XX-XXXX-XXXX",
+#     "+Z-XX-XXX-XXX-XXXX",
+#     "+ZZ-XX-XXXX-XXXX",
+# ]
+#
