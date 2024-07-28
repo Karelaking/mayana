64660f4ae365639fe8ab70fe4633c8ecf1dd3cdc
mradul kumar katiyar <karelaking277@gmail.com>
2024-07-29 01:54:50 +0530

    Completed name.py file work

--- mayana/fake/name.py
+++ mayana/fake/name.py
@@ -1,6 +1,6 @@
-from secrets import choice
-from random import shuffle
 from pathlib import Path
+from random import choice
+from typing import List
 
 from utils.pickler import Loader
 
@@ -8,92 +8,86 @@ loader: Loader = Loader()
 
 
 class Name:
+    """
+    Generates a fake name based on a given name format string.
+    """
     def __init__(self, count: int = 1, country: str = "India"):
         self.__name_data = loader.load_data(file_path=str(Path.joinpath(Path(__file__).parent.parent.parent,
                                                                         "constants/name.json")))
         self.__name_parts_data = loader.load_data(file_path=str(Path.joinpath(Path(__file__).parent.parent.parent,
-                                                                        "constants/name_parts.json")))
-        # self.__name_data = loader.load_data(file_path="constants/name.json")
-        # self.__name_parts_data = loader.load_data(file_path="constants/name_parts.json")
+                                                                              "constants/name_parts.json")))
         self.count: int = count
         self.country: str = country
+        self._title: List[str] = self.__name_parts_data["title"]
+        self._prefix: List[str] = self.__name_parts_data["prefix"]
+        self._first: List[str] = self.__name_data[country]["first_names"]
+        self._middel: List[str] = self.__name_data[country]["middle_names"]
+        self._last: List[str] = self.__name_data[country]["last_names"]
+        self._suffix: List[str] = self.__name_parts_data["suffix"]
 
     def __iter__(self):
         yield self
 
-    def __next__(self):
-        """
-        Returns the next item from the iterator.
+    def _generate_name(self, __name_format: str = "f M l", __count: int = 1) -> str:
+        _patterns = {
+            "t": lambda: choice(self._title).lower(),
+            "p": lambda: choice(self._prefix).lower(),
+            "f": lambda: choice(self._first).lower(),
+            "m": lambda: choice(self._middel).lower(),
+            "l": lambda: choice(self._last).lower(),
+            "s": lambda: choice(self._suffix).lower(),
+            "T": lambda: choice(self._title).capitalize(),
+            "P": lambda: choice(self._prefix).capitalize(),
+            "F": lambda: choice(self._first).capitalize(),
+            "M": lambda: choice(self._middel).capitalize(),
+            "L": lambda: choice(self._last).capitalize(),
+            "S": lambda: choice(self._suffix).capitalize(),
+        }
 
-        Raises:
-            StopIteration: If there are no more items to return.
-        """
-        ...
+        _full_name: str = ""
 
-    def _generate_name(self, first: bool, middel: bool, last: bool, suffix: bool, prefix: bool, title: bool,
-                       country: str):
-        __full_name: str = ""
-        __title: str = self.__name_parts_data["title"]
-        __prefix: str = self.__name_parts_data["prefix"]
-        __first: str = self.__name_data[country]["first_names"]
-        __middel: str = self.__name_data[country]["middle_names"]
-        __last: str = self.__name_data[country]["last_names"]
-        __suffix: str = self.__name_parts_data["suffix"]
+        for char in __name_format:
+            if char in _patterns:
+                _full_name += _patterns[char]()
+            else:
+                _full_name += char
+        return _full_name
 
-        __full_name += choice(__title) if title else str()
-        __full_name += " " + choice(__prefix) if prefix else str()
-        __full_name += " " + choice(__first) if first else str()
-        __full_name += " " + choice(__middel) if middel else str()
-        __full_name += " " + choice(__last) if last else str()
-        __full_name += choice(__suffix) if suffix else str()
-
-        return __full_name.strip()
-
-    def name(self, first: bool = True, middel: bool = False, last: bool = False, suffix: bool = False,
-             prefix: bool = False, title: bool = False, country: str = "India"):
-
+    def name(self, name_format: str):
         for _ in range(self.count):
-            yield self._generate_name(first=first, title=title, prefix=prefix, middel=middel, last=last, suffix=suffix,
-                                      country=country)
+            yield self._generate_name(__name_format=name_format)
 
     @property
     def get_name(self):
         for _ in range(self.count):
-            yield self._generate_name(title=False, prefix=False, first=True, middel=False, last=True, suffix=False,
-                                      country=self.country)
+            yield self._generate_name("F L")
 
     @property
     def get_first_name(self):
         for _ in range(self.count):
-            yield self._generate_name(title=False, prefix=False, first=True, middel=False, last=False, suffix=False,
-                                      country=self.country)
+            yield self._generate_name("F")
 
     @property
     def get_last_name(self):
         for _ in range(self.count):
-            yield self._generate_name(title=False, prefix=False, first=False, middel=False, last=True, suffix=False,
-                                      country=self.country)
+            yield self._generate_name("L")
 
     @property
     def get_name_with_title(self):
         for _ in range(self.count):
-            yield self._generate_name(title=True, prefix=False, first=True, middel=False, last=True, suffix=False,
-                                      country=self.country)
+            yield self._generate_name("T F L")
 
     @property
     def get_name_with_prefix(self):
         for _ in range(self.count):
-            yield self._generate_name(title=False, prefix=True, first=True, middel=False, last=True, suffix=False,
-                                      country=self.country)
+            yield self._generate_name("P F L")
 
     @property
     def get_name_with_suffix(self):
         for _ in range(self.count):
-            yield self._generate_name(title=False, prefix=False, first=True, middel=False, last=True, suffix=True,
-                                      country=self.country)
+            yield self._generate_name("F L S")
 
     @property
     def get_name_with_middel(self):
         for _ in range(self.count):
-            yield self._generate_name(title=False, prefix=False, first=True, middel=True, last=True, suffix=False,
-                                      country=self.country)
+            yield self._generate_name("F M L")
