9f66d113b1c7e1b7d19a76bd1a01d23ce478fcc5
mradul kumar katiyar <karelaking277@gmail.com>
2024-07-29 20:15:46 +0530

    Completed country.py, pin_code.py, placeholder.py.

--- mayana/fake/phone.py
+++ mayana/fake/phone.py
@@ -1,57 +1,48 @@
-from typing import List
 from random import randint
 
-# Define patterns for different placeholders
-patterns = {
-    "X": lambda: str(randint(0, 9)),
-    "N": lambda: str(randint(2, 9)),  # For non-zero leading digit area codes
-    "Z": lambda: str(randint(1, 9)),  # For country codes
-}
 
+class Phone:
+    def __init__(self, count: int = 1):
+        self.__count: int = count
+        # Define patterns for different placeholders
+        self.__patterns = {
+            "X": lambda: str(randint(0, 9)),
+            "N": lambda: str(randint(2, 9)),  # For non-zero leading digit area codes
+            "Z": lambda: str(randint(1, 9)),  # For country codes
+        }
 
-# Generate the phone number based on the format string
-def generate_random_phone(format_string: str, patterns: dict) -> str:
-    phone_number = ""
-    for char in format_string:
-        if char in patterns:
-            phone_number += patterns[char]()
-        else:
-            phone_number += char
-    return phone_number
+    def _generate_phone(self, format_string: str) -> str:
+        phone_number = ""
+        for char in format_string:
+            if char in self.__patterns:
+                phone_number += self.__patterns[char]()
+            else:
+                phone_number += char
+        return phone_number
 
+    def phone(self, phone_format: str = "+ZZ XXX-XXX-XXXX"):
+        """
+        Generate a specified number of phone numbers in the given format.
 
-def phone(format_string: str = "+ZZ XXX-XXX-XXXX", count: int = 1) -> str | List[str]:
-    """
+        :param phone_format: The format for the phone numbers.
+        :return: A generator of phone numbers.
 
-    :param format_string:
-    :param count:
-    :return:
-
-    formats = [
-    "XXX-XXX-XXXX",
-    "(XXX) XXX-XXXX",
-    "XXX.XXX.XXXX",
-    "XXX XXX XXXX",
-    "+X-XXX-XXX-XXXX",
-    "+XX-XXXX-XXXX",
-    "+Z-XX-XXX-XXX-XXXX",
-    "+ZZ-XX-XXXX-XXXX",
-]
-    """
-
-    if count != 1:
-        return [
-            generate_random_phone(format_string=format_string, patterns=patterns)
-            for _ in range(count)
+        formats = [
+            "XXX-XXX-XXXX",
+            "(XXX) XXX-XXXX",
+            "XXX.XXX.XXXX",
+            "XXX XXX XXXX",
+            "+X-XXX-XXX-XXXX",
+            "+XX-XXXX-XXXX",
+            "+Z-XX-XXX-XXX-XXXX",
+            "+ZZ-XX-XXXX-XXXX",
         ]
 
-    else:
-        return generate_random_phone(format_string=format_string, patterns=patterns)
+        """
+        for _ in range(self.__count):
+            yield self._generate_phone(format_string=phone_format)
 
-
-if __name__ == "__main__":
-    print(phone())
-
-# Example usage
-
-#
+    @property
+    def get_phone(self):
+        for _ in range(self.__count):
+            yield self._generate_phone(format_string="+ZZ XXX-XXX-XXXX")
